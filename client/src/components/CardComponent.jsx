284947ac6dadb5219210e64eda024f968da500eb
Mahmud Mai <mahmudx00@gmail.com>
2023-06-02 20:42:23 +0100

    Ref: remove console.logs

--- client/src/components/CardComponent.jsx
+++ client/src/components/CardComponent.jsx
@@ -11,14 +11,6 @@ import {
 } from '@chakra-ui/react';
 
 const CardComponent = props => {
-  console.log(
-    '🚀 ~ file: CardComponent.jsx:14 ~ CardComponent ~ props:',
-    props
-  );
-  console.log(
-    '🚀 ~ file: CardComponent.jsx:28 ~ CardComponent ~ props.btnArray:',
-    props.btnArray
-  );
   return (
     <Card mx={'auto'} maxW="xl" variant={'elevated'} align={'center'} p={5}>
       <Heading as={'h2'}>{props.title}</Heading>
