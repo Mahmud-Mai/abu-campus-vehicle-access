b79ccce4548ee0fd711577a48a208f993ccf91f2
Mahmud Mai <mahmudx00@gmail.com>
2023-06-02 12:02:46 +0100

    ref: refactoring Camera box

--- client/src/pages/checkIn/CheckIn.jsx
+++ client/src/pages/checkIn/CheckIn.jsx
@@ -1,7 +1,86 @@
-import React from 'react';
+import React, { useCallback, useRef, useState } from 'react';
+import {
+  Box,
+  Button,
+  ButtonGroup,
+  Card,
+  CardBody,
+  CardFooter,
+  Heading,
+} from '@chakra-ui/react';
+import CamComponent from '../../components/CamComponent';
+import CardComponent from '../../components/CardComponent';
 
-const CheckIn = () => {
-  return <div>CheckIn Page</div>;
+const videoConstraints = {
+  width: 400,
+  height: 400,
+  facingMode: 'environment',
 };
 
+const CheckIn = () => {
+  const [picture, setPicture] = useState('');
+  const webcamRef = useRef(null);
+
+  const capture = useCallback(() => {
+    const pictureSrc = webcamRef.current.getScreenshot();
+    setPicture(pictureSrc);
+  }, [webcamRef]);
+
+  return (
+    <Box>
+      {picture === '' ? (
+        <Box>
+          <CardComponent
+            title={'SNAP PLATE NUMBER'}
+            btnAction={capture}
+            btnText={'Snap'}
+          >
+            <CamComponent
+              webcamRef={webcamRef}
+              videoConstraints={videoConstraints}
+            />
+          </CardComponent>
+        </Box>
+      ) : (
+        // <Card mx={'auto'} maxW="xl" variant={'elevated'} align={'center'} p={5}>
+        //   <Heading as={'h2'}>SNAP PLATE NUMBER</Heading>
+        //   <CardBody>
+        //     <CamComponent
+        //       webcamRef={webcamRef}
+        //       videoConstraints={videoConstraints}
+        //     />
+        //   </CardBody>
+        //   <CardFooter>
+        //     <Button onClick={capture}>Snap Picture</Button>
+        //   </CardFooter>
+        // </Card>
+        <Card
+          mx={'auto'}
+          maxW={'xl'}
+          variant={'elevated'}
+          align={'center'}
+          sx={{ transition: 0.4 }}
+          p={5}
+        >
+          <Heading as={'h2'}>IMAGE PREVIEW</Heading>
+          <CardBody>
+            <img src={picture} alt="plate number img" />
+          </CardBody>
+          <CardFooter>
+            <ButtonGroup>
+              <Button
+                onClick={() => {
+                  setPicture('');
+                }}
+              >
+                Retake Picture
+              </Button>
+              <Button>Generate Ticket</Button>
+            </ButtonGroup>
+          </CardFooter>
+        </Card>
+      )}
+    </Box>
+  );
+};
 export default CheckIn;
