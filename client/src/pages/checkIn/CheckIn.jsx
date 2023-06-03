3daa689830b13929fa560085a94865d0f8374524
Mahmud Mai <mahmudx00@gmail.com>
2023-06-03 22:31:25 +0100

    Ref: more refactoring and documenting of code

--- client/src/pages/checkIn/CheckIn.jsx
+++ client/src/pages/checkIn/CheckIn.jsx
@@ -1,5 +1,5 @@
 import React, { useCallback, useEffect, useRef, useState } from 'react';
-import { Box, Text } from '@chakra-ui/react';
+import { Box, Button, ButtonGroup, Text } from '@chakra-ui/react';
 import CamComponent from '../../components/CamComponent';
 import CardComponent from '../../components/CardComponent';
 import { createWorker } from 'tesseract.js';
@@ -10,21 +10,23 @@ const videoConstraints = {
   facingMode: 'environment',
 };
 
-const worker = await createWorker({
-  logger: m => console.log(m),
-});
+const worker = await createWorker();
 
 const CheckIn = () => {
-  const [picture, setPicture] = useState('');
+  const [plateNumberImage, setPlateNumberImage] = useState('');
+  const [plateNumber, setPlateNumber] = useState('');
+  const [isDummy, setIsDummy] = useState(false);
+  const [isPNValid, setIsPNValid] = useState(false); // for plate number validation
   const webcamRef = useRef(null);
 
   const capture = useCallback(() => {
     const pictureSrc = webcamRef.current.getScreenshot();
-    setPicture(pictureSrc);
+    setPlateNumberImage(pictureSrc);
   }, [webcamRef]);
 
   const reCapture = () => {
-    setPicture('');
+    setPlateNumberImage('');
+    setIsDummy(false);
   };
 
   const btnArray = [
@@ -39,26 +41,50 @@ const CheckIn = () => {
     },
   ];
 
-  const doOCR = async picture => {
-    if (picture) {
-      await worker.load();
-      await worker.loadLanguage('eng');
-      await worker.initialize('eng');
-      const {
-        data: { text },
-      } = await worker.recognize(picture);
-      setOcr(text);
-    }
+  const doOCR = useCallback(async () => {
+    await worker.load();
+    await worker.loadLanguage('eng');
+    await worker.initialize('eng');
+    const {
+      data: { text },
+    } = await worker.recognize(plateNumberImage);
+    setPlateNumber(text);
+  }, [plateNumberImage]);
+
+  const dummyPlateOcr = () => {
+    const dummyUrl = process.env.local.DUMMY_URL;
+    doOCR(dummyUrl);
   };
 
-  const [ocr, setOcr] = useState('Recognizing...');
-  useEffect(() => {
-    doOCR(picture);
-  }, [picture]);
+  const dummyPlateTicket = () => {
+    setPlateNumber('YLA-291AP');
+  };
 
+  useEffect(() => {
+    doOCR();
+  }, [doOCR, plateNumberImage]);
+
+  const validatePlateNumber = plateNumber => {
+    const regex = /^([A-Z]{3})(\d{1,2})([A-Z]{1,3})(\d{1,4})$/;
+    return regex.test(plateNumber);
+  };
+
+  const handleChange = event => {
+    const plateNumber = event.target.value;
+    isPNValid(validatePlateNumber(plateNumber));
+
+    if (isPNValid) {
+      console.log('The plate number matches a Nigerian plate number format.');
+    } else {
+      console.log(
+        'The plate number does not match a Nigerian plate number format.'
+      );
+    }
+  };
   return (
     <Box>
-      {picture === '' ? (
+      {plateNumberImage === '' ? (
+        // If plateNumberImage is empty: Show Video-Camera Component
         <Box>
           <CardComponent
             title={'SNAP PLATE NUMBER'}
@@ -72,14 +98,32 @@ const CheckIn = () => {
           </CardComponent>
         </Box>
       ) : (
+        // If plateNumberImage is not empty: Show Preview Component
         <Box>
           <CardComponent title={'PREVIEW PLATE NUMBER'} props={btnArray}>
-            <img src={picture} alt="plate number img" />
-            <Text>Recognized Text: {ocr}</Text>
+            <Box>
+              <img src={plateNumberImage} alt="plate number img" />
+            </Box>
+            <Text m={3} onChange={handleChange}>
+              Recognized Text: {plateNumber}
+            </Text>
+            {!isDummy ? (
+              <ButtonGroup>
+                <Button onClick={() => setIsDummy(true)}>Use Dummy Data</Button>
+              </ButtonGroup>
+            ) : (
+              <ButtonGroup>
+                <Button onClick={dummyPlateOcr}>Dummy Plate for OCR</Button>
+                <Button onClick={dummyPlateTicket}>
+                  Dummy Plate for ticket
+                </Button>
+              </ButtonGroup>
+            )}
           </CardComponent>
         </Box>
       )}
     </Box>
   );
 };
+
 export default CheckIn;
