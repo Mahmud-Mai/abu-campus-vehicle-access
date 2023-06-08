34340f0c0f4016a8ea1d1841219c4d4625b23d5b
Mahmud Mai <mahmudx00@gmail.com>
2023-06-08 19:40:33 +0100

    Feat: create DataTable component

---
+++ client/src/components/DataTable.jsx
@@ -0,0 +1,30 @@
+import React, { useMemo } from 'react';
+import { Box, Flex } from '@chakra-ui/react';
+import { AgGridReact } from 'ag-grid-react';
+import 'ag-grid-community/styles/ag-grid.css';
+import 'ag-grid-community/styles/ag-theme-alpine.css';
+
+const DataTable = ({ rowData, columnDefs }) => {
+  // DefaultColDef sets props common to all Columns
+  const defaultColDef = useMemo(() => ({ sortable: true }), []);
+
+  return (
+    <Flex>
+      <Box
+        mx={'auto'}
+        className="ag-theme-alpine"
+        sx={{ width: '5xl', height: 700, textAlign: 'left' }}
+      >
+        <AgGridReact
+          rowData={rowData}
+          columnDefs={columnDefs}
+          defaultColDef={defaultColDef}
+          animateRows={true}
+          rowSelection="multiple"
+        />
+      </Box>
+    </Flex>
+  );
+};
+
+export default DataTable;
