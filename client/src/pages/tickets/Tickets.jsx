d5cd24eba4d275a485e6f80fed5617a5b7db4e3d
Mahmud Mai <mahmudx00@gmail.com>
2023-06-08 19:42:05 +0100

    Feat: display DataTable complonent

--- client/src/pages/tickets/Tickets.jsx
+++ client/src/pages/tickets/Tickets.jsx
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
+import DataTable from '../../components/DataTable';
+// import { ticketRows, ticketCols } from '../../database/db';
+import { useSelector } from 'react-redux';
 
 const Tickets = () => {
-  return <div>Tickets Page</div>;
+  const tickets = useSelector(state => state.ticket);
+  const [rowData] = useState(tickets.ticketRows);
+  const [columnDefs] = useState(tickets.ticketCols);
+
+  return (
+    <div>
+      <DataTable rowData={rowData} columnDefs={columnDefs} />
+    </div>
+  );
 };
 
 export default Tickets;
