79366c621616aa84abaef786bf1b3cd1a2758184
Mahmud Mai <mahmudx00@gmail.com>
2023-06-10 10:53:06 +0100

    Fix: qrcode text encoding not working

--- client/src/components/QrcodeComponent.jsx
+++ client/src/components/QrcodeComponent.jsx
@@ -1,17 +1,23 @@
 import QRCode from 'qrcode';
 import { useEffect, useRef } from 'react';
 
-export default function QRCodeComponent() {
+export default function QrcodeComponent({ ticketId }) {
   const canvasRef = useRef();
 
-  useEffect(ticketId => {
+  useEffect(() => {
     QRCode.toCanvas(
       canvasRef.current,
       ticketId || ' ',
       { width: 180 },
-      error => error && console.error(error)
+      error => {
+        if (error) {
+          console.error(error);
+        } else {
+          console.log(`QRCode Success!!`);
+        }
+      }
     );
-  }, []);
+  }, [ticketId]);
 
   return <canvas ref={canvasRef} />;
 }
