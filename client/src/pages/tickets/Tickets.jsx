0af193c852841918fea62e3713e1875c03a6c4da
Mahmud Mai <mahmudx00@gmail.com>
2023-06-10 15:28:00 +0100

    Feat: implement fetching and displaying Tickets info on a Data Table

--- client/src/pages/tickets/Tickets.jsx
+++ client/src/pages/tickets/Tickets.jsx
@@ -1,12 +1,47 @@
-import React, { useState } from 'react';
 import DataTable from '../../components/DataTable';
 import { useSelector } from 'react-redux';
 import { selectAllTickets } from '../../features/ticket/ticketsSlice';
 
+// const camelCase = str => {
+//   if (typeof str !== 'string') {
+//     return '';
+//   }
+
+//   return str.replace(/[_-]/g, '');
+// };
+
 const Tickets = () => {
-  const tickets = useSelector(selectAllTickets);
-  const [rowData] = useState(tickets.ticketRows);
-  const [columnDefs] = useState(tickets.ticketCols);
+  const parkingTicketsArray = useSelector(selectAllTickets);
+
+  const rowData = parkingTicketsArray.map(
+    ({ ticketId, plateNumber, userId, gateId, date, time }) => ({
+      'Ticket Id': ticketId,
+      'Plate Numbers': plateNumber,
+      'Entry Gate': Number(gateId),
+      Time: time,
+      Date: date,
+      'Personnel ID': userId,
+    })
+  );
+  console.log('🚀 ~ file: Tickets.jsx:26 ~ Tickets ~ rowData:', rowData);
+
+  // const parkingTicketTableColumns = Object.keys(parkingTicketsArray[0]);
+
+  // const columnDefs = parkingTicketTableColumns.map(item => {
+  //   const fieldName = item;
+  //   return {
+  //     field: fieldName,
+  //   };
+  // });
+
+  const columnDefs = [
+    { field: 'Ticket Id' },
+    { field: 'Plate Numbers', filter: true },
+    { field: 'Entry Gate', filter: true },
+    { field: 'Time' },
+    { field: 'Date', filter: true },
+    { field: 'Personnel ID', filter: true },
+  ];
 
   return (
     <div>
