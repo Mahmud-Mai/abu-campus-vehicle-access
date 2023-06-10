b22189c8d88b216d706b5423e91501e3321d68cf
Mahmud Mai <mahmudx00@gmail.com>
2023-06-10 14:34:18 +0100

    Feat: add spnner to CamComponent

--- client/src/components/CamComponent.jsx
+++ client/src/components/CamComponent.jsx
@@ -1,17 +1,38 @@
-import React from 'react';
+import { Flex, Spinner } from '@chakra-ui/react';
+import { useEffect, useState } from 'react';
 import Webcam from 'react-webcam';
 
 const CamComponent = ({ webcamRef, videoConstraints }) => {
+  const [isStreaming, setIsStreaming] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsStreaming(true);
+    }, 1500);
+  }, []);
+
   return (
     <>
-      <Webcam
-        audio={false}
-        height={400}
-        ref={webcamRef}
-        width={400}
-        screenshotFormat="image/jpeg"
-        videoConstraints={videoConstraints}
-      />
+      {isStreaming ? (
+        <Webcam
+          audio={false}
+          height={400}
+          ref={webcamRef}
+          width={400}
+          screenshotFormat="image/jpeg"
+          videoConstraints={videoConstraints}
+        />
+      ) : (
+        <Flex align={'center'} height={200}>
+          <Spinner
+            thickness="4px"
+            speed="0.65s"
+            emptyColor="gray.200"
+            color="blue.500"
+            size="xl"
+          />
+        </Flex>
+      )}
     </>
   );
 };
