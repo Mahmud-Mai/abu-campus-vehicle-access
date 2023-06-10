4a3a234b755dddd8a8e29fe8a390c9faed3a7b8b
Mahmud Mai <mahmudx00@gmail.com>
2023-06-10 15:28:43 +0100

    chore: include mock data to ticketSlice

--- client/src/features/ticket/ticketsSlice.js
+++ client/src/features/ticket/ticketsSlice.js
@@ -1,6 +1,39 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-const initialState = [];
+const initialState = [
+  {
+    ticketId: '8pPV5Owt4DOo26xEIpZGe',
+    plateNumber: 'ABC-123AA',
+    userId: 0,
+    gateId: 0,
+    date: '10/06/2023',
+    time: '10:22:03',
+  },
+  {
+    ticketId: 'p6mdfI44AuIhGDZaansc4',
+    plateNumber: 'DEC-123DC',
+    userId: 2,
+    gateId: '2',
+    date: '10/06/2023',
+    time: '11:51:29',
+  },
+  {
+    ticketId: 'BXvA0bgWqwvMicHX5eeTA',
+    plateNumber: 'ADE-012AD',
+    userId: 0,
+    gateId: '3',
+    date: '10/06/2023',
+    time: '11:51:46',
+  },
+  {
+    ticketId: '08Gdp3_n1dp-hzucGcku-',
+    plateNumber: 'AEF-345AE',
+    userId: 1,
+    gateId: '4',
+    date: '10/06/2023',
+    time: '11:51:58',
+  },
+];
 
 export const ticketsSlice = createSlice({
   name: 'tickets',
