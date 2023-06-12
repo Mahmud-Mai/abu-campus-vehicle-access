e42fdc85d00f12e55874e092e0b7676da59be41e
Mahmud Mai <mahmudx00@gmail.com>
2023-06-12 22:11:42 +0100

    Feat: setup the basic UI

--- client/src/pages/checkOut/CheckOut.jsx
+++ client/src/pages/checkOut/CheckOut.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import { Box } from '@chakra-ui/react';
+import PageHeading from '../../components/PageHeading';
+import CardComponent from '../../components/CardComponent';
+import CamComponent from '../../components/CamComponent';
 
 const CheckOut = () => {
-  return <div>Check out page</div>;
+  return (
+    <Box height={'100vh'}>
+      <PageHeading
+        title={'Vehicle Check-Out'}
+        subTitle={'Scan Vehicle Plate Numbers and Grant them exit'}
+      />
+      <Box height={'85vh'}>
+        <CardComponent btnText={'Scan QrCode'}>
+          <CamComponent />
+        </CardComponent>
+      </Box>
+    </Box>
+  );
 };
 
 export default CheckOut;
