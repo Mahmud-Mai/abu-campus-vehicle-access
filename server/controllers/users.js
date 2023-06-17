65abde0c371391472b0e26c5874199707f50e1e8
Mahmud Mai <mahmudx00@gmail.com>
2023-06-17 18:54:41 +0100

    Feat: write logic for updating users, plus other fixes

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,14 +1,18 @@
-import User from "../model/User.js";
 import asyncHandler from "express-async-handler";
 import bcrypt from "bcryptjs";
+import User from "../models/User.js";
+import Ticket from "../models/Ticket.js";
 
 // @desc Get all users
 // @route GET /users
 // @access Private
 export const getAllUsers = asyncHandler(async (req, res) => {
   const users = await User.find().select("-password").lean();
-  // Confirm data
-  if (!users) return res.status(400).json({ message: "No users found" });
+
+  // Check if users exists before send all users
+  if (!users?.length)
+    return res.status(400).json({ message: "No users found" });
+
   res.status(200).json(users);
 });
 
@@ -17,15 +21,9 @@ export const getAllUsers = asyncHandler(async (req, res) => {
 // @access Private
 export const createUser = asyncHandler(async (req, res) => {
   const { userName, email, password, role, active } = req.body;
+
   // Confirm data
-  if (
-    !userName ||
-    !email ||
-    !password ||
-    !Array.isArray(role) ||
-    !role.length ||
-    !active
-  ) {
+  if (!userName || !email || !password || !role?.length || !active) {
     return res.status(400).json({ message: "All fields are required" });
   }
 
@@ -37,8 +35,8 @@ export const createUser = asyncHandler(async (req, res) => {
   // Hash password and create user
   const hashedPwd = await bcrypt.hash(password, 10);
   const userObject = { userName, email, password: hashedPwd, role, active };
-  const newUser = await User.create(userObject);
 
+  const newUser = await User.create(userObject);
   if (newUser) {
     res
       .status(201)
@@ -52,45 +50,56 @@ export const createUser = asyncHandler(async (req, res) => {
 // @route Update /users
 // @access Private
 export const updateUser = asyncHandler(async (req, res) => {
-  const { id, userName, email, password, role, active } = req.body;
-  // Confrim data
-  if (
-    !id ||
-    !userName ||
-    !email ||
-    !password ||
-    !Array.isArray(role) ||
-    !role.lenth ||
-    !active
-  ) {
-    return res.status(400).json({ message: "All fields are required" });
+  const id = req.params.id;
+  const { userName, email, password, role, active } = req.body;
+
+  // Confrim data id
+  if (!userName || !email || !role?.length || active === null || undefined) {
+    return res.status(400).json({ message: "Required fields are missing" });
   }
 
   // Confirm useer exists
-  const user = await User.findById(id);
+  const user = await User.findById(id).exec();
   if (!user) return res.status(409).json({ message: "User does not exist" });
 
-  User.userName = userName;
-  User.role = role;
-  User.active = active;
-
+  // Create User Object
+  const userObject = { userName, role, active };
   if (password) {
     // Hash password
-
-    User.password = await bcrypt.hash(password, 10);
+    userObject.password = await bcrypt.hash(password, 10);
   }
 
   // Save updated user
-  const updatedUser = await User.save();
-
+  const updatedUser = await User.findByIdAndUpdate(id, userObject);
   res
     .status(201)
-    .json({ message: `${updateUser.userName} was updated succesfully` });
+    .json({ message: `${updatedUser.userName} was updated succesfully` });
 });
-
-// Save updated user
 
 // @desc delete a user
 // @route Delete /users
 // @access Private
-export const deleteUser = asyncHandler(async (req, res) => {});
+export const deleteUser = asyncHandler(async (req, res) => {
+  const id = req.params.id;
+
+  // Confirm user data
+  if (!id) return res.status(400).json({ message: "User Id is not provided" });
+
+  // Check if user has created tickets
+  const ticketsUserHasCreated = await Ticket.findOne({ user: id });
+  if (ticketsUserHasCreated)
+    return res.status(400).json({
+      message: "Can't delete user because, User has created some tickets",
+    });
+
+  // delete user if user exists
+  const user = await User.findById(id);
+  if (!user)
+    return res.status(400).json({ message: "Can't find a user with that id" });
+  const preDeletedUser = user;
+  await User.deleteOne(user);
+
+  res.status(200).json({
+    message: `Deleted user: ${preDeletedUser.userName} with id: ${preDeletedUser._id} successfully`,
+  });
+});
