db262f93157abb82cfb9c2308aae759f66f501fe
Mahmud Mai <mahmudx00@gmail.com>
2023-06-18 14:05:24 +0100

    Feat: create updateBlacklist  controller

--- server/controllers/blacklist.js
+++ server/controllers/blacklist.js
@@ -1,5 +1,6 @@
 import asyncHandler from "express-async-handler";
 import Blacklist from "../models/Blacklist";
+import Vehicle from "../models/Vehicle";
 
 // @desc Get all blacklist
 // @route GET /blacklists
@@ -25,7 +26,7 @@ export const getBlacklist = asyncHandler(async (req, res) => {
     return res.status(400).json({ message: "Please provide a valid Id" });
 
   // check For a match
-  const blacklist = await Blacklist.findById(id);
+  const blacklist = await Blacklist.findById(id).lean();
   if (!blacklist)
     return res.status(400).json({ message: "No blacklist matches that Id" });
 
@@ -36,12 +37,51 @@ export const getBlacklist = asyncHandler(async (req, res) => {
 // @desc Create a blacklist
 // @route Post /blacklists
 // @access Private
-// export const createBlacklist = asyncHandler(async (req, res) => {});
+export const createBlacklist = asyncHandler(async (req, res) => {
+  const { vehicle, status, reason } = req.body;
+
+  // Validate user data
+  if (!vehicle || !status || !reason)
+    return res.status(400).json({ message: "Please provide required fields" });
+
+  // Check for a match
+  const vehicleToBlacklist = await Vehicle.findById(vehicle).lean();
+  if (!vehicleToBlacklist)
+    return res.status(400).json({ message: "No vehicle matches that Id" });
+
+  // Create document
+  const blacklistObject = { vehicle, status, reason };
+  const result = await Blacklist.create({ blacklistObject });
+
+  // Return result
+  res.status(200).json(result);
+});
 
 // @desc update a blacklist
 // @route Update /blacklists/:id
 // @access Private
-// export const updateBlacklist = asyncHandler (async (req, res) => {})
+export const updateBlacklist = asyncHandler(async (req, res) => {
+  const id = req.params.id;
+  const { status, reason } = req.body;
+
+  // Validate user data
+  if (!id)
+    return res.status(400).json({ message: "Please provide a valid Id" });
+  if (!status || !reason)
+    return res.status(400).json({ message: "Please provide required fields" });
+
+  // Check for a mactch
+  const blacklistToUpdate = await Blacklist.findById(id).lean();
+  if (!blacklistToUpdate)
+    return res.status(400).json({ message: "No blacklist matches that Id" });
+
+  // Update document
+  const blacklistObject = { status, reason };
+  const result = await Blacklist.findByIdAndUpdate(id, blacklistObject);
+
+  // Return result
+  res.status(200).json(result);
+});
 
 // @desc delete a blacklist
 // @route Delete /blacklists/:id
