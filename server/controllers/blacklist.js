9f145592dfed2f3e8efe44fd3de67d3d3baf4c35
Mahmud Mai <mahmudx00@gmail.com>
2023-06-18 13:28:38 +0100

    Feat: create getAllBlacklists controller

---
+++ server/controllers/blacklist.js
@@ -0,0 +1,27 @@
+import asyncHandler from "express-async-handler";
+import Blacklist from "../models/Blacklist";
+
+// @desc Get all blacklist
+// @route GET /blacklists
+// @access Private
+export const getAllBlacklists = asyncHandler(async (req, res) => {});
+
+// @desc Get a Specific blacklist
+// @route GET /blacklists/:id
+// @access Private
+// export const getBlacklist = asyncHandler (async (req, res) => {})
+
+// @desc Create a blacklist
+// @route Post /blacklists
+// @access Private
+// export const createBlacklist = asyncHandler(async (req, res) => {});
+
+// @desc update a blacklist
+// @route Update /blacklists/:id
+// @access Private
+// export const updateBlacklist = asyncHandler (async (req, res) => {})
+
+// @desc delete a blacklist
+// @route Delete /blacklists/:id
+// @access Private
+// export const deleteBlacklist = asyncHandler(async (req, res) => {});
