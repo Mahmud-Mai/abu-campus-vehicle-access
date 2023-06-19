4bd13d8094bbedd2b1ae4618a14a93f87b9473e4
Mahmud Mai <mahmudx00@gmail.com>
2023-06-19 13:25:21 +0100

    Feat: create getAllTickets event handler

--- server/controllers/tickets.js
+++ server/controllers/tickets.js
@@ -1,58 +1,73 @@
 import { Schema } from "mongoose";
-import Ticket from "../model/Ticket.js";
-import Vehicle from "../model/Vehicle.js";
+import Ticket from "../models/Ticket.js";
+import Vehicle from "../models/Vehicle.js";
+import asyncHandler from "express-async-handler";
+asyncHandler;
 
-export const getAllTickets = async (req, res) => {
-  res.status(200).send("getAllTickets is yet to be implemented");
-  // try {
-  //   const ticket = await Ticket.find();
-  //   res.send(200).json(tickets);
-  // } catch (error) {
-  //   res.send(400).json({ message: error });
-  // }
-};
+// @desc Get all gates
+// @route GET /gates
+// @access Private
+export const getAllTickets = asyncHandler(async (req, res) => {
+  const tickets = await Ticket.find();
 
-export const createTicket = async (req, res) => {
-  // const newTicket = await Ticket.create(req.body);
-  // res.json(newTicket);
+  // Check tickets eist
+  if (!tickets?.length)
+    return res.status(400).json({ message: "Tickets list is empty" });
 
-  const plateNoOfInterest = req.body.plateNo;
-  const vehicleOfInterest = await Vehicle.findOne({
-    plateNumber: plateNoOfInterest,
-  });
+  // Populate user and gate fields
+  const populatedTickets = await Promise.all(
+    tickets.map(async (ticket) => {
+      // Used separate .populate bcoz chained .populate() kept giving errors
+      await ticket.populate({ path: "plateNumber", select: "plateNumber" });
+      await ticket.populate({ path: "user", select: "userName" });
+      await ticket.populate({ path: "gate", select: "gateName" });
 
-  if (vehicleOfInterest) {
-    res.send("Yes VOI exists: " + vehicleOfInterest + " " + true);
-  } else {
-    res.send("No VOI does not exist: " + vehicleOfInterest + " " + false);
-  }
-  // const newTicket = await Ticket.create({
-  //   plateNumber: plateNoOfInterest,
-  // userId: {
-  //   type: Schema.Types.ObjectId,
-  //   ref: "User",
-  //   required: true,
-  // },
-  // gateId: {
-  //   type: Schema.Types.ObjectId,
-  //   ref: "Gate",
-  //   required: true,
-  // },
-  // });
-  //   res.status(200).json(newTicket);
-  // } else {
-  //   const newVehicle = await Vehicle.create({
-  //     plateNumber: plateNoOfInterest,
-  //   });
+      const plateNumber = ticket.plateNumber
+        ? ticket.plateNumber.plateNumber
+        : null;
+      const user = ticket.user ? ticket.user.userName : null;
+      const gate = ticket.gate ? ticket.gate.gateName : null;
+      return {
+        ...ticket.toObject(),
+        plateNumber,
+        gate,
+        user,
+      };
+    })
+  );
 
-  //   const newTicket = await Ticket.create({
-  //     plateNumber: plateNoOfInterest,
-  //   });
+  // Return response
+  res.status(200).json(populatedTickets);
+});
 
-  //   res
-  //     .status(200)
-  //     .json(
-  //       `New vehicle created: \n ${newVehicle}. \nAnd new ticket created: \n${newTicket}`
-  //     );
-  // }
-};
+// @desc Create a gate
+// @route Post /gates
+// @access Private
+// export const createTicket = asyncHandler(async (req, res) => {
+//   // const newTicket = await Ticket.create(req.body);
+//   // res.json(newTicket);
+
+//   const plateNoOfInterest = req.body.plateNo;
+//   const vehicleOfInterest = await Vehicle.findOne({
+//     plateNumber: plateNoOfInterest,
+//   });
+
+//   if (vehicleOfInterest) {
+//     res.send("Yes VOI exists: " + vehicleOfInterest + " " + true);
+//   } else {
+//     res.send("No VOI does not exist: " + vehicleOfInterest + " " + false);
+//   }
+// });
+
+// @desc Get a Specific gate
+// @route GET /gates/:id
+// @access Private
+// export const getTicket = asyncHandler( async (req, res) => {})
+
+// @desc update a gate
+// @route Update /gates/:id
+// @access Private
+// @desc delete a gate
+// @route Delete /gates/:id
+// @access Private
+// export const getTicket = asyncHandler( async (req, res) => {})
