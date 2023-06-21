04bab3d9e0b35e008a7ab552b84f6cc06ae44e79
Mahmud Mai <mahmudx00@gmail.com>
2023-06-21 15:59:19 +0100

    Undo: recover mistakenly deleted gates file

---
+++ server/controllers/gates.js
@@ -0,0 +1,110 @@
+import asyncHandler from "express-async-handler";
+import Gate from "../models/Gate.js";
+
+// @desc Get all gates
+// @route GET /gates
+// @access Private
+export const getAllGates = asyncHandler(async (req, res) => {
+  const gates = await Gate.find().lean();
+
+  // Confirm gates exists before returning result
+  if (!gates?.length)
+    return res.status(400).json({ message: "No gates found" });
+
+  res.status(200).json(gates);
+});
+
+// @desc Get a Specific gate
+// @route GET /gates/:id
+// @access Private
+export const getGate = asyncHandler(async (req, res) => {
+  const id = req.params.id;
+
+  // Validate user data
+  if (!id)
+    return res.status(400).json({ message: "Please provide a valid Id" });
+
+  // Check for a match
+  const gate = await Gate.findById(id).lean();
+  if (!gate)
+    return res.status(400).json({ message: "No gate matches that Id" });
+
+  // Return results
+  res.status(200).json(gate);
+});
+
+// @desc Create a gate
+// @route Post /gates
+// @access Private
+export const createGate = asyncHandler(async (req, res) => {
+  const { gateName } = req.body;
+
+  // Validate user data
+  if (!gateName)
+    return res.status(400).json({ message: "Gatename was not provided" });
+
+  // Check for duplicates
+  const duplicate = await Gate.findOne({ gateName: gateName });
+  if (duplicate)
+    return res
+      .status(409)
+      .json({ message: "Duplicates not allowed. Gatename is already in Use" });
+
+  // Create Gate, then return results
+  const newGate = await Gate.create({ gateName });
+  res.status(201).json({ message: `${newGate.gateName} created successfully` });
+});
+
+// @desc update a gate
+// @route Update /gates/:id
+// @access Private
+export const updateGate = asyncHandler(async (req, res) => {
+  const id = req.params.id;
+  const { gateName } = req.body;
+
+  // Validate user data
+  if (!id)
+    return res.status(400).json({ message: "Please provide a valid Id" });
+  if (!gateName)
+    return res.status(400).json({ message: "Gatename was not provided" });
+
+  // Check for duplicates: to ensure new value is different from old value
+  const duplicate = await Gate.findOne({ gateName: gateName });
+  if (duplicate)
+    return res
+      .status(409)
+      .json({ message: "Duplicates not allowed. Gatename is already in Use" });
+
+  // Update model
+  const updatedGate = await Gate.findByIdAndUpdate(id, { gateName });
+
+  // Send response
+  if (updatedGate)
+    return res
+      .status(201)
+      .json({ message: `${gateName} was updated succesfully` });
+});
+
+// @desc delete a gate
+// @route Delete /gates/:id
+// @access Private
+export const deleteGate = asyncHandler(async (req, res) => {
+  const id = req.params.id;
+
+  // Validate user data
+  if (!id)
+    return res.status(400).json({ message: "Please provide a valid Id" });
+  // Check for a match
+  const gateToBeDeleted = await Gate.findById(id);
+  if (!gateToBeDeleted)
+    return res.status(400).json({ message: "No gate matches that Id" });
+
+  // Delete document
+  const deletedGate = await Gate.deleteOne(gateToBeDeleted);
+
+  // Send response
+  if (deletedGate)
+    return res
+      .status(201)
+      .json({ message: `${gateToBeDeleted.gateName} was deleted succesfully` });
+});
