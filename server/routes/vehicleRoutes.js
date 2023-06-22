bd570e94a42023b518c1819231d1b992755d3a77
Mahmud Mai <mahmudx00@gmail.com>
2023-06-22 12:16:44 +0100

    Feat: create route for getting vehicles by plate number

--- server/routes/vehicleRoutes.js
+++ server/routes/vehicleRoutes.js
@@ -4,11 +4,13 @@ import {
   deleteVehicle,
   getAllVehicles,
   getVehicle,
+  getVehicleByPlateNumber,
   updateVehicle,
 } from "../controllers/vehicles.js";
 
 const router = express.Router();
 
 router.route("/").get(getAllVehicles).post(createVehice);
+router.route("/by-plate-number").get(getVehicleByPlateNumber);
 router.route("/:id").get(getVehicle).patch(updateVehicle).delete(deleteVehicle);
 export default router;
