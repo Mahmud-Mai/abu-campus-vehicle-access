310c683522e64f979617ca762bbcdf66d39254e5
Mahmud Mai <mahmudx00@gmail.com>
2023-06-23 11:32:12 +0100

    Fix: add conditions to check tickets list exist before fetching tickets

--- client/src/pages/tickets/index.jsx
+++ client/src/pages/tickets/index.jsx
@@ -3,29 +3,29 @@ import DataTable from '../../components/DataTable';
 import { useDispatch, useSelector } from 'react-redux';
 import {
   fetchTickets,
-  // getTicketsError,
-  getTicketsStatus,
+  // getTicketsListError,
+  getTicketsListStatus,
   fetchAllTickets,
 } from '../../features/ticket/ticketsSlice';
 import PageHeading from '../../components/PageHeading';
-import { Flex, Spinner, Text } from '@chakra-ui/react';
+import { Flex } from '@chakra-ui/react';
 
 const Tickets = () => {
   const dispatch = useDispatch();
 
   const ticketsList = useSelector(fetchAllTickets);
-  const ticketsStatus = useSelector(getTicketsStatus);
-  // const ticketsError = useSelector(getTicketsError);
+  const ticketsStatus = useSelector(getTicketsListStatus);
 
-  const rowData = ticketsList.tickets.map(
-    ({ _id, plateNumber, user, gate, ticketStatus }) => ({
-      'Ticket Id': _id || null,
-      'Plate Number': plateNumber || null,
-      'Entry Gate': gate,
-      PassageType: ticketStatus,
-      'Personnel InCharge': user,
-    })
-  );
+  const rowData =
+    typeof ticketsList === 'object' && ticketsList?.length
+      ? ticketsList.map(({ _id, plateNumber, user, gate, ticketStatus }) => ({
+          'Ticket Id': _id || null,
+          'Plate Number': plateNumber || null,
+          'Entry Gate': gate,
+          PassageType: ticketStatus,
+          'Personnel InCharge': user,
+        }))
+      : null;
 
   const columnDefs = [
     { field: 'Ticket Id' },
@@ -40,24 +40,6 @@ const Tickets = () => {
     ticketsStatus === 'idle' && dispatch(fetchTickets());
   }, [dispatch, ticketsStatus]);
 
-  // Decide content to display
-  let content;
-  if (ticketsStatus === 'loading') {
-    content = (
-      <Spinner
-        thickness="4px"
-        speed="0.65s"
-        emptyColor="gray.200"
-        color="blue.500"
-        size="xl"
-      />
-    );
-  } else if (ticketsStatus === 'success') {
-    content = <DataTable rowData={rowData} columnDefs={columnDefs} />;
-  } else if (ticketsStatus === 'failed') {
-    content = <Text>Could not fetch Tickets from the Backend</Text>;
-  }
-
   return (
     <>
       <PageHeading
@@ -65,7 +47,7 @@ const Tickets = () => {
         subTitle={'View all tickets information'}
       />
       <Flex mx={'auto'} justify={'center'}>
-        {content}
+        <DataTable rowData={rowData} columnDefs={columnDefs} />;
       </Flex>
     </>
   );
