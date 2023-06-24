2251e1b97c6e1cc5e895fcc4b9299d2481a103b6
Mahmud Mai <mahmudx00@gmail.com>
2023-06-24 09:08:25 +0100

    Fix: write correct condition to check if the action.payload is not equal to the strings 'Request failed with status code 400' or 'Request failed with status code 500'

--- client/src/features/vehicle/vehicleSlice.js
+++ client/src/features/vehicle/vehicleSlice.js
@@ -1,15 +1,105 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
 
 const initialState = {
-  vehicleId: '',
-  vehiclePlateNo: '',
-  blacklistStatus: '',
-  firstEntry: '',
+  vehicles: [],
+  status: 'idle',
+  error: null,
 };
 
-const vehicleSlice = createSlice({
-  name: 'vehicle',
+const url = 'http://localhost:5001/api/v1/vehicles/by-plate-number';
+
+export const fetchVehicles = createAsyncThunk(
+  'vehicle/fetchVehicles',
+  async () => {
+    try {
+      const response = await axios.get(url);
+      console.log(`🚀 ~ fetchVehicles ~ response:`, response);
+      return [...response.data];
+    } catch (error) {
+      return error.message;
+    }
+  }
+);
+
+export const fetchVehicleByPlateNumber = createAsyncThunk(
+  'vehicles/fetchVehicleByPlateNumber',
+  async plateNumber => {
+    const response = await axios.get(
+      `http://localhost:5001/api/v1/vehicles/by-plate-number/?plateNumber=${plateNumber}`
+    );
+
+    return response.data;
+  }
+);
+
+export const createVehicleByPlateNumber = createAsyncThunk(
+  'vehicles/createVehicleByPlateNumber',
+  async plateNumber => {
+    try {
+      const response = await axios.post(
+        `http://localhost:5001/api/v1/vehicles/by-plate-number/?plateNumber=${plateNumber}`
+      );
+      console.log(
+        `🚀 ~ createVehicleByPlateNumber ~ response.data:`,
+        response.data
+      );
+      return response.data;
+    } catch (error) {
+      return error.message;
+    }
+  }
+);
+
+export const vehiclesSlice = createSlice({
+  name: 'vehicles',
   initialState,
+  reducers: {
+    createVehicle: {
+      reducer(state, action) {
+        state.vehicles = action.payload;
+      },
+      prepare(plateNumber, blacklistStatus) {
+        return {
+          payload: { plateNumber, blacklistStatus },
+        };
+      },
+    },
+  },
+  extraReducers: {
+    [fetchVehicles.pending]: state => {
+      state.status = 'loading';
+    },
+    [fetchVehicles.fulfilled]: (state, action) => {
+      state.status = 'success';
+      state.vehicles = action.payload;
+    },
+    [fetchVehicles.rejected]: (state, action) => {
+      state.status = 'failed';
+      state.error = action.error.message;
+    },
+    [fetchVehicleByPlateNumber.fulfilled]: (state, action) => {
+      state.status = 'success';
+      const responseError =
+        action.payload === 'Request failed with status code 400' ||
+        action.payload === 'Request failed with status code 500';
+      if (!responseError) {
+        state.vehicles.push(action.payload);
+      }
+    },
+    [createVehicleByPlateNumber.fulfilled]: (state, action) => {
+      state.status = 'success';
+      const responseError =
+        action.payload === 'Request failed with status code 400' ||
+        action.payload === 'Request failed with status code 500';
+      if (!responseError) {
+        state.vehicles.push(action.payload);
+      }
+    },
+  },
 });
 
-export default vehicleSlice.reducer;
+export const fetchAllVehicles = state => state.vehicles.vehicles;
+export const vehicleListStatus = state => state.vehicles.status;
+export const { createVehicle } = vehiclesSlice.actions;
+export default vehiclesSlice.reducer;
