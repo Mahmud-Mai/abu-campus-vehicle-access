74416510ae7c961579bff3c80ed9ed001a003d9b
Mahmud Mai <mahmudx00@gmail.com>
2023-07-02 08:41:31 +0100

    Feat: move api calls into a separate file

--- client/src/features/gate/gateSlice.js
+++ client/src/features/gate/gateSlice.js
@@ -1,22 +1,10 @@
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import axios from 'axios';
+import { createSlice } from '@reduxjs/toolkit';
+import { fetchGates } from '../../api/gates';
 
 const initialState = {
   gates: [],
   status: 'idle',
 };
-
-const baseUrl = process.env.REACT_APP_BASE_URL;
-const url = `${baseUrl}/gates`;
-
-export const fetchGates = createAsyncThunk('gates/fetchGates', async () => {
-  try {
-    const response = await axios.get(url);
-    return [...response.data];
-  } catch (error) {
-    return error.message;
-  }
-});
 
 export const gatesSlice = createSlice({
   name: 'gates',
--- client/src/features/vehicle/vehicleSlice.js
+++ client/src/features/vehicle/vehicleSlice.js
@@ -1,56 +1,15 @@
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import axios from 'axios';
+import { createSlice } from '@reduxjs/toolkit';
+import {
+  createVehicleByPlateNumber,
+  fetchVehicleByPlateNumber,
+  fetchVehicles,
+} from '../../api/vehicles';
 
 const initialState = {
   vehicles: [],
   status: 'idle',
   error: null,
 };
-
-const baseUrl = process.env.REACT_APP_BASE_URL;
-const url = `${baseUrl}/vehicles`;
-
-export const fetchVehicles = createAsyncThunk(
-  'vehicle/fetchVehicles',
-  async () => {
-    try {
-      const response = await axios.get(`${url}/by-plate-number`);
-      console.log(`🚀 ~ fetchVehicles ~ response:`, response);
-      return [...response.data];
-    } catch (error) {
-      return error.message;
-    }
-  }
-);
-
-export const fetchVehicleByPlateNumber = createAsyncThunk(
-  'vehicles/fetchVehicleByPlateNumber',
-  async plateNumber => {
-    const response = await axios.get(
-      `${url}/by-plate-number/?plateNumber=${plateNumber}`
-    );
-
-    return response.data;
-  }
-);
-
-export const createVehicleByPlateNumber = createAsyncThunk(
-  'vehicles/createVehicleByPlateNumber',
-  async plateNumber => {
-    try {
-      const response = await axios.post(
-        `${url}/by-plate-number/?plateNumber=${plateNumber}`
-      );
-      console.log(
-        `🚀 ~ createVehicleByPlateNumber ~ response.data:`,
-        response.data
-      );
-      return response.data;
-    } catch (error) {
-      return error.message;
-    }
-  }
-);
 
 export const vehiclesSlice = createSlice({
   name: 'vehicles',
