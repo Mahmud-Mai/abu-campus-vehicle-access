555129f99c8d52d7556ff9577238c5f870d72c4c
Mahmud Mai <mahmudx00@gmail.com>
2023-07-03 22:37:06 +0100

    Ref: change bracket notation of extraReducers to use builder notation

--- client/src/features/vehicle/vehicleSlice.js
+++ client/src/features/vehicle/vehicleSlice.js
@@ -26,36 +26,37 @@ export const vehiclesSlice = createSlice({
       },
     },
   },
-  extraReducers: {
-    [fetchVehicles.pending]: state => {
-      state.status = 'loading';
-    },
-    [fetchVehicles.fulfilled]: (state, action) => {
-      state.status = 'success';
-      state.vehicles = action.payload;
-    },
-    [fetchVehicles.rejected]: (state, action) => {
-      state.status = 'failed';
-      state.error = action.error.message;
-    },
-    [fetchVehicleByPlateNumber.fulfilled]: (state, action) => {
-      state.status = 'success';
-      const responseError =
-        action.payload === 'Request failed with status code 400' ||
-        action.payload === 'Request failed with status code 500';
-      if (!responseError) {
-        state.vehicles.push(action.payload);
-      }
-    },
-    [createVehicleByPlateNumber.fulfilled]: (state, action) => {
-      state.status = 'success';
-      const responseError =
-        action.payload === 'Request failed with status code 400' ||
-        action.payload === 'Request failed with status code 500';
-      if (!responseError) {
-        state.vehicles.push(action.payload);
-      }
-    },
+  extraReducers: builder => {
+    builder
+      .addCase(fetchVehicles.pending, state => {
+        state.status = 'loading';
+      })
+      .addCase(fetchVehicles.fulfilled, (state, action) => {
+        state.status = 'success';
+        state.vehicles = action.payload;
+      })
+      .addCase(fetchVehicles.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.error.message;
+      })
+      .addCase(fetchVehicleByPlateNumber.fulfilled, (state, action) => {
+        state.status = 'success';
+        const responseError =
+          action.payload === 'Request failed with status code 400' ||
+          action.payload === 'Request failed with status code 500';
+        if (!responseError) {
+          state.vehicles.push(action.payload);
+        }
+      })
+      .addCase(createVehicleByPlateNumber.fulfilled, (state, action) => {
+        state.status = 'success';
+        const responseError =
+          action.payload === 'Request failed with status code 400' ||
+          action.payload === 'Request failed with status code 500';
+        if (!responseError) {
+          state.vehicles.push(action.payload);
+        }
+      });
   },
 });
 
