d1f66416d06aff75366123165badd0b86748b483
Mahmud Mai <mahmudx00@gmail.com>
2023-07-05 21:39:19 +0100

    chore: log fetchGates url to the console

--- client/src/api/gates.js
+++ client/src/api/gates.js
@@ -7,6 +7,8 @@ const url = `${baseUrl}/gates`;
 export const fetchGates = createAsyncThunk('gates/fetchGates', async () => {
   try {
     const response = await axios.get(url);
+    console.log(`🚀 ~ fetchGates ~ url:`, url);
+    console.log(`🚀 ~ fetchGates ~ response:`, response);
     return response.data;
   } catch (error) {
     console.log(error.toJSON());
