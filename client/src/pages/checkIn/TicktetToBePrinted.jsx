89450382fd33684e378e5d3105eca6193b2aefec
Mahmud Mai <mahmudx00@gmail.com>
2023-10-02 13:58:28 +0100

    Use static ticket info temporarily

--- client/src/pages/checkIn/TicktetToBePrinted.jsx
+++ client/src/pages/checkIn/TicktetToBePrinted.jsx
@@ -27,41 +27,87 @@ import {
 
 const TicktetToBePrinted = () => {
   const NewTicket = useSelector(fetchNewlyCreatedTicket);
+  console.log(`🚀 ~ TicktetToBePrinted ~ NewTicket:`, NewTicket);
   const lastTicket = NewTicket?.slice(-1)[0];
   console.log('🚀 ~ TicktetToBePrinted ~ lastTicket:', lastTicket);
 
+  const ticket = {
+    ticketId: 'fadfbv0124nmk990',
+    plateNo: 'YLA-291AP',
+    entryTime: '07:11',
+    entryDate: '05/06/2023',
+    exitTime: '10:15',
+    exitDate: '05/06/2023',
+    entryType: 'Entry',
+    entryGate: 'North Gate',
+    exitGate: 'Main Gate',
+    personelIncharge: 'Sambo Hamis',
+  };
+
   let content;
-  if (getTicketsListStatus !== 'loading') {
+  if (lastTicket) {
     content = (
       <Stack divider={<StackDivider />} width={'24rem'} spacing="5">
         <Flex justify={'space-between'}>
           <Box>Ticket Id:</Box>
-          <Text> {lastTicket._id}</Text>
+          <Text> {ticket.ticketId}</Text>
         </Flex>
         <Flex justify={'space-between'}>
           <Box>Vehicle Plate Number:</Box>
-          <Text> {lastTicket.plateNumber}</Text>
+          <Text> {ticket.plateNo}</Text>
         </Flex>
         <Flex justify={'space-between'}>
           <Box>Passage Type</Box>
-          <Text> {lastTicket.ticketStatus}</Text>
+          <Text> {ticket.entryType}</Text>
         </Flex>
         <Flex justify={'space-between'}>
           <Box>Gate Used:</Box>
-          <Text>{lastTicket.gate}</Text>
+          <Text>{ticket.entryGate}</Text>
         </Flex>
         <Flex justify={'space-between'}>
           <Box>Time:</Box>
-          <Text>{lastTicket.createdAt}</Text>
+          <Text>
+            {ticket.entryDate} | {ticket.exitTime}
+          </Text>
         </Flex>
         <Flex justify={'space-between'}>
           <Box>Security Personnel:</Box>
-          <Text>{lastTicket.user}</Text>
+          <Text>{ticket.personelIncharge}</Text>
         </Flex>
         <Flex justify={'center'}>
-          <QrcodeComponent ticketId={lastTicket._id} />
+          <QrcodeComponent ticketId={ticket.id} />
         </Flex>
       </Stack>
+
+      // <Stack divider={<StackDivider />} width={'24rem'} spacing="5">
+      //   <Flex justify={'space-between'}>
+      //     <Box>Ticket Id:</Box>
+      //     <Text> {lastTicket._id}</Text>
+      //   </Flex>
+      //   <Flex justify={'space-between'}>
+      //     <Box>Vehicle Plate Number:</Box>
+      //     <Text> {lastTicket.plateNumber}</Text>
+      //   </Flex>
+      //   <Flex justify={'space-between'}>
+      //     <Box>Passage Type</Box>
+      //     <Text> {lastTicket.ticketStatus}</Text>
+      //   </Flex>
+      //   <Flex justify={'space-between'}>
+      //     <Box>Gate Used:</Box>
+      //     <Text>{lastTicket.gate}</Text>
+      //   </Flex>
+      //   <Flex justify={'space-between'}>
+      //     <Box>Time:</Box>
+      //     <Text>{lastTicket.createdAt}</Text>
+      //   </Flex>
+      //   <Flex justify={'space-between'}>
+      //     <Box>Security Personnel:</Box>
+      //     <Text>{lastTicket.user}</Text>
+      //   </Flex>
+      //   <Flex justify={'center'}>
+      //     <QrcodeComponent ticketId={lastTicket._id} />
+      //   </Flex>
+      // </Stack>
     );
   } else {
     content = (
